6249c0536f07364ba2538a552c05b869fba8729a
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2024-03-29 18:36:36 -0400

    added homePage file

---
+++ Frontend/centbot-aws/src/pages/homepage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/homePage.tsx</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
